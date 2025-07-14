be88c7c97ea20e3fbfefacd98c31f2191db62952
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-07-14 12:44:37 +0000

    feat: add reusable auth form

--- frontend/src/app/shared/components/auth-form/auth-form.component.ts
+++ frontend/src/app/shared/components/auth-form/auth-form.component.ts
@@ -1,10 +1,59 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import {
+  FormBuilder,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+} from '@angular/forms';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-auth-form',
   standalone: true,
-  imports: [],
+  imports: [
+    CommonModule,
+    ReactiveFormsModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatButtonModule,
+    MatIconModule,
+  ],
   templateUrl: './auth-form.component.html',
-  styleUrl: './auth-form.component.scss',
+  styleUrls: ['./auth-form.component.scss'],
 })
-export class AuthFormComponent {}
+export class AuthFormComponent {
+  @Input() isRegister = false;
+  @Output() formSubmit = new EventEmitter<any>();
+
+  form: FormGroup;
+
+  hide = true;
+
+  constructor(private fb: FormBuilder) {
+    this.form = this.fb.group({
+      name: [''],
+      username: [''],
+      email: ['', [Validators.required, Validators.email]],
+      password: ['', [Validators.required, Validators.minLength(6)]],
+    });
+  }
+
+  ngOnInit() {
+    if (!this.isRegister) {
+      this.form.removeControl('name');
+      this.form.removeControl('username');
+    }
+  }
+
+  onSubmit() {
+    if (this.form.valid) {
+      this.formSubmit.emit(this.form.value);
+    } else {
+      this.form.markAllAsTouched();
+    }
+  }
+}
