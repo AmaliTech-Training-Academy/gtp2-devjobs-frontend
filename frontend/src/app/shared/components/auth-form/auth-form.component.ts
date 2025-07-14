ab500be37d2f556438bccffac072f478dd2dd67e
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-07-14 11:51:28 +0000

    feat: generate reusable auth-form component

--- frontend/src/app/shared/components/auth-form/auth-form.component.ts
+++ frontend/src/app/shared/components/auth-form/auth-form.component.ts
@@ -2,10 +2,9 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-auth-form',
+  standalone: true,
   imports: [],
   templateUrl: './auth-form.component.html',
-  styleUrl: './auth-form.component.scss'
+  styleUrl: './auth-form.component.scss',
 })
-export class AuthFormComponent {
-
-}
+export class AuthFormComponent {}
