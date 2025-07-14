2d806a1e58615c8bad58bd7dfdc8eb376ed60869
Jummai <jummyhassan5@gmail.com>
2025-07-14 17:09:48 +0000

    add job description section

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,3 +1,12 @@
 import { Routes } from '@angular/router';
+import { JobDetailsComponent } from './features/job-details/job-details.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: '',
+    loadComponent: () =>
+      import('./features/job-details/job-details.component').then(
+        (m) => m.JobDetailsComponent
+      ),
+  },
+];
