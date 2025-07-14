6a2a17833f67f8722ee02de9cbe534f05d449d1a
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-07-14 13:41:22 +0000

    feat: create  model to handle user sign in and login

---
+++ frontend/src/app/model/auth.model.ts
@@ -0,0 +1,28 @@
+export interface LoginRequest {
+  email: string;
+  password: string;
+}
+
+export interface RegisterRequest {
+  name?: string;
+  username: string;
+  email: string;
+  password: string;
+  companyName?: string;
+  companyEmail?: string;
+}
+
+export interface User {
+  id: number;
+  name: string;
+  email: string;
+  role: 'admin' | 'job_seeker' | 'employer';
+}
+
+export interface LoginResponse {
+  status: 'success' | 'error';
+  data: {
+    token: string;
+    user: User;
+  };
+}
