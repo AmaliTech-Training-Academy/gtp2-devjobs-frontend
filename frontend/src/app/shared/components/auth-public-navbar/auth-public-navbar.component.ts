a080f47d3d091c95ddddd5416c448cac01b124b2
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-07-15 18:03:14 +0000

    feat: add hamburger icon and implement functionality for navbar responsiveness

--- frontend/src/app/shared/components/auth-public-navbar/auth-public-navbar.component.ts
+++ frontend/src/app/shared/components/auth-public-navbar/auth-public-navbar.component.ts
@@ -2,12 +2,23 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-public-auth-navbar',
   standalone: true,
-  imports: [CommonModule, RouterModule, MatButtonModule],
+  imports: [CommonModule, RouterModule, MatButtonModule, MatIconModule],
   templateUrl: './auth-public-navbar.component.html',
   styleUrls: ['./auth-public-navbar.component.scss'],
 })
-export class AuthPublicNavbarComponent {}
+export class AuthPublicNavbarComponent {
+  isMobileMenuOpen = false;
+
+  toggleMobileMenu() {
+    this.isMobileMenuOpen = !this.isMobileMenuOpen;
+  }
+
+  closeMobileMenu() {
+    this.isMobileMenuOpen = false;
+  }
+}
