e7b054cbb75a086ac163ec6182e5e32ae4652a3d
samuelBright09 <samuelbrightsk@gmail.com>
2025-07-15 14:35:06 +0000

    Feat: Add job seeker dashboard. (#4)
    * Feat: Add job seeker dashboard.
    
    * Fix: Rectified lazy loaded route to lod the right component on request.
    
    ---------
    
    Co-authored-by: EmannDev <emmanueldwayneaborsinsiah@gmail.com>

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -1,8 +1,11 @@
 import { Routes } from '@angular/router';
+
 import { RegisterComponent } from './features/auth/pages/register/register/register.component';
 import { LoginComponent } from './features/auth/pages/login/login.component';
 import { JobDetailsComponent } from './features/job-details/job-details.component';
 import { ApplicationFormComponent } from './features/application-form/application-form.component';
+import { SeekerDashboardComponent } from './layouts/seeker/seeker-dashboard/seeker-dashboard.component';
+import { JobListComponent } from './features/jobs/job-list/job-list.component';
 
 export const routes: Routes = [
    {
@@ -27,5 +30,14 @@ export const routes: Routes = [
         (m) => m.ApplicationFormComponent
       ),
   },
-
+     {
+        path: 'seeker/dashboard', 
+        loadComponent: () => import('./layouts/seeker/seeker-dashboard/seeker-dashboard.component').then(m => m.SeekerDashboardComponent),
+        canActivate: [],
+        children: [
+            { path: '', component: JobListComponent,
+                canActivate: []
+             },
+        ],
+    },
 ];
