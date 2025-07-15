a4d0d3e83b45b93fdb39469f9cd5a89dbcaf5ad0
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-07-15 15:49:46 +0000

    feat: add path for forgot-password to navigate to page and implement styling for the template

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -3,9 +3,10 @@ import { RegisterComponent } from './features/auth/pages/register/register/regis
 import { LoginComponent } from './features/auth/pages/login/login.component';
 import { JobDetailsComponent } from './features/job-details/job-details.component';
 import { ApplicationFormComponent } from './features/application-form/application-form.component';
+import { ForgotPasswordComponent } from './features/auth/pages/forgot-password/forgot-password/forgot-password.component';
 
 export const routes: Routes = [
-   {
+  {
     path: '',
     loadComponent: () =>
       import('./features/job-details/job-details.component').then(
@@ -20,12 +21,15 @@ export const routes: Routes = [
     path: 'login',
     component: LoginComponent,
   },
-   {
+  {
+    path: 'forgot-password',
+    component: ForgotPasswordComponent,
+  },
+  {
     path: 'application-form',
     loadComponent: () =>
       import('./features/application-form/application-form.component').then(
         (m) => m.ApplicationFormComponent
       ),
   },
-
 ];
