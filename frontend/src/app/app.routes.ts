033c48b037417d89cc1bf1430d6d0237ea95888e
Jummai <jummyhassan5@gmail.com>
2025-07-16 15:17:54 +0000

    create reusable job-content and structured routes

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -2,27 +2,46 @@ import { Routes } from '@angular/router';
 
 import { RegisterComponent } from './features/auth/pages/register/register/register.component';
 import { LoginComponent } from './features/auth/pages/login/login.component';
-import { JobDetailsComponent } from './features/job-details/job-details.component';
-import { ApplicationFormComponent } from './features/application-form/application-form.component';
-
-import { EmployerDashboardComponent } from './features/employer-dashboard/employer-dashboard.component';
 import { EmployerLayoutComponent } from './features/employer-layout/employer-layout.component';
-import { EmployerJobsComponent } from './features/employer-jobs/employer-jobs.component';
-import { EmployerApplicationsComponent } from './features/employer-applications/employer-applications.component';
-import { EmployerSettingsComponent } from './features/employer-settings/employer-settings.component';
-
-import { SeekerDashboardComponent } from './layouts/seeker/seeker-dashboard/seeker-dashboard.component';
 import { JobListComponent } from './features/jobs/job-list/job-list.component';
 
-
 export const routes: Routes = [
-   {
+  {
     path: '',
-    loadComponent: () =>
-      import('./features/job-details/job-details.component').then(
-        (m) => m.JobDetailsComponent
-      ),
+    redirectTo: '/seeker/dashboard',
+    pathMatch: 'full',
   },
+  {
+    path: 'seeker/dashboard',
+    loadComponent: () =>
+      import(
+        './layouts/seeker/seeker-dashboard/seeker-dashboard.component'
+      ).then((m) => m.SeekerDashboardComponent),
+    canActivate: [],
+    children: [
+      {
+        path: '',
+        component: JobListComponent,
+        canActivate: [],
+        pathMatch: 'full',
+      },
+      {
+        path: 'job-details',
+        loadComponent: () =>
+          import('./features/job-details/job-details.component').then(
+            (m) => m.JobDetailsComponent
+          ),
+      },
+      {
+        path: 'application-form',
+        loadComponent: () =>
+          import('./features/application-form/application-form.component').then(
+            (m) => m.ApplicationFormComponent
+          ),
+      },
+    ],
+  },
+
   {
     path: 'register',
     component: RegisterComponent,
@@ -31,48 +50,43 @@ export const routes: Routes = [
     path: 'login',
     component: LoginComponent,
   },
-   {
-    path: 'application-form',
-    loadComponent: () =>
-      import('./features/application-form/application-form.component').then(
-        (m) => m.ApplicationFormComponent
-      ),
-  },
+
   {
     path: 'employer',
     component: EmployerLayoutComponent,
     children: [
       {
         path: 'dashboard',
-        loadComponent: () => import('./features/employer-dashboard/employer-dashboard.component').then( m => m.EmployerDashboardComponent ),
-        title: 'Employer dashboard'
+        loadComponent: () =>
+          import(
+            './features/employer-dashboard/employer-dashboard.component'
+          ).then((m) => m.EmployerDashboardComponent),
+        title: 'Employer dashboard',
       },
       {
         path: 'jobs',
-        loadComponent: () => import('./features/employer-jobs/employer-jobs.component').then( m => m.EmployerJobsComponent ),
-        title: 'Employer Jobs'
+        loadComponent: () =>
+          import('./features/employer-jobs/employer-jobs.component').then(
+            (m) => m.EmployerJobsComponent
+          ),
+        title: 'Employer Jobs',
       },
       {
         path: 'applications',
-        loadComponent: () => import('./features/employer-applications/employer-applications.component').then( m => m.EmployerApplicationsComponent ),
-        title: 'Employer Applications'
+        loadComponent: () =>
+          import(
+            './features/employer-applications/employer-applications.component'
+          ).then((m) => m.EmployerApplicationsComponent),
+        title: 'Employer Applications',
       },
       {
         path: 'settings',
-        loadComponent: () => import('./features/employer-settings/employer-settings.component').then( m => m.EmployerSettingsComponent),
-        title: 'Employer Settings'
-      }
-    ]
-
+        loadComponent: () =>
+          import(
+            './features/employer-settings/employer-settings.component'
+          ).then((m) => m.EmployerSettingsComponent),
+        title: 'Employer Settings',
+      },
+    ],
   },
-     {
-        path: 'seeker/dashboard', 
-        loadComponent: () => import('./layouts/seeker/seeker-dashboard/seeker-dashboard.component').then(m => m.SeekerDashboardComponent),
-        canActivate: [],
-        children: [
-            { path: '', component: JobListComponent,
-                canActivate: []
-             },
-        ],
-    },
 ];
--- frontend/src/app/components/job-card/job-card.component.ts
+++ frontend/src/app/components/job-card/job-card.component.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { CommonModule } from '@angular/common';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-job-card',
   imports: [CommonModule, CardModule],
   templateUrl: './job-card.component.html',
-  styleUrls: ['./job-card.component.scss']
+  styleUrls: ['./job-card.component.scss'],
 })
-export class JobCardComponent {}
+export class JobCardComponent {
+  router = inject(Router);
+
+  onCardClick() {
+    this.router.navigate(['seeker/dashboard/job-details']);
+  }
+}
