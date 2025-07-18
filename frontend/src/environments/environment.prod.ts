549dae0927147abaae8e79b70165062265079025
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-07-18 16:02:20 +0000

    feat: add fallback urls within the environments

--- frontend/src/environments/environment.prod.ts
+++ frontend/src/environments/environment.prod.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: process.env['NG_APP_BASE_URL'],
+  apiUrl:
+    process.env['NG_APP_BASE_URL'] ||
+    'http://gtp2-be-testing-alb-114080375.eu-central-1.elb.amazonaws.com',
 };
