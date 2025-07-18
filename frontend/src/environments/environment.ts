808e0b2454dc961743882abf31a1b7a41060ffc9
emanndev <emmanueldwayneaborsinsiah@gmail.com>
2025-07-18 16:02:20 +0000

    feat: add fallback urls within the environments

--- frontend/src/environments/environment.ts
+++ frontend/src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: false,
-  apiUrl: process.env['NG_APP_BASE_URL'],
+  apiUrl:
+    process.env['NG_APP_BASE_URL'] || 'https://f20c1106ab65.ngrok-free.app',
 };
