1f775a9db79fc8886834b32766e85f1b246fc72d
Larry N. N. Williams <42799384+larryking01@users.noreply.github.com>
2025-07-19 23:52:28 +0000

    Feature: Add functionality for applications page. (#73)
    * Feature: Integrate Backend and try posting a job
    
    * Feature: Add search functionality to data table
    
    * FIX: Implement Employer Data view Table
    
    * feature/larry/datatables done
    
    * Feature: Implement delete modal on employer jobs display table
    
    * chore: syncing with development
    
    * Chore: Successfully fetched jobs and posted jobs
    
    * Chore: refactored employer job creation process
    
    * Feature: Add Table To Allow Employer To Toggle Status Of Job Application
    
    * chore: align employer initials in profile div of employer navbar properly
    
    * Chore: Refactore user profile options popover on employer navbar

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -180,8 +180,8 @@ export const routes: Routes = [
     component: UnathorizedComponent,
   },
   // Catch-all Wildcard
-  {
-    path: '**',
-    component: PageNotFoundComponent,
-  },
+  // {
+  //   path: '**',
+  //   component: PageNotFoundComponent,
+  // },
 ];
--- frontend/src/app/features/employer-jobs/employer-jobs.component.ts
+++ frontend/src/app/features/employer-jobs/employer-jobs.component.ts
@@ -2,110 +2,39 @@ import { Component, inject } from '@angular/core';
 import { ButtonModule } from 'primeng/button';
 import { CreateJobModalComponent } from '../../shared/create-job-modal/create-job-modal.component';
 import { EmptyStateComponent } from '../../shared/empty-state/empty-state.component';
-import { DataTableComponent } from '../../shared/data-table/data-table.component';
 import { ModalsServiceService } from '../../core/services/modalsService/modals-service.service';
 import { JobDetailsModalComponent } from '../../shared/job-details-modal/job-details-modal.component';
-
+import { ActionsDataTableComponent } from '../../shared/actions-data-table/actions-data-table.component';
 import { EmployerHttpRequestsService } from '../../core/services/employerJobCRUDService/employer-http-requests.service';
+import { ActionModalComponent } from '../../components/action-modal/action-modal.component';
 
 
 @Component({
   selector: 'app-employer-jobs',
-  imports: [ ButtonModule, CreateJobModalComponent, EmptyStateComponent, JobDetailsModalComponent, DataTableComponent ],
+  imports: [ ButtonModule, CreateJobModalComponent, 
+             EmptyStateComponent, JobDetailsModalComponent, 
+             ActionsDataTableComponent, ActionModalComponent ],
   templateUrl: './employer-jobs.component.html',
   styleUrl: './employer-jobs.component.scss'
 })
 export class EmployerJobsComponent {
 
   modalService = inject( ModalsServiceService )
-
   employerHttp = inject( EmployerHttpRequestsService )
 
+  showDeleteModal: boolean = false;
 
-  properties: any [] = ["Job Title", "Job Type", "Date", "Salary", "Location", "Action"]
 
-  jobs = [
-    {
-      "Job Title": "Frontend Developer",
-      "Job Type": "Full-Time",
-      "Date": "2025-07-10",
-      "Salary": 60000,
-      "Location": "Accra, Ghana",
-      "Action": "View"
-    },
-    {
-      "Job Title": "Backend Engineer",
-      "Job Type": "Contract",
-      "Date": "2025-07-08",
-      "Salary": 70000,
-      "Location": "Kumasi, Ghana",
-      "Action": "View"
-    },
-    {
-      "Job Title": "UI/UX Designer",
-      "Job Type": "Part-Time",
-      "Date": "2025-07-05",
-      "Salary": 45000,
-      "Location": "Lagos, Nigeria",
-      "Action": "View"
-    },
-    {
-      "Job Title": "DevOps Engineer",
-      "Job Type": "Full-Time",
-      "Date": "2025-07-01",
-      "Salary": 80000,
-      "Location": "Nairobi, Kenya",
-      "Action": "View"
-    },
-    {
-      "Job Title": "Mobile Developer",
-      "Job Type": "Internship",
-      "Date": "2025-06-28",
-      "Salary": 20000,
-      "Location": "Cape Town, South Africa",
-      "Action": "View"
-    },
-    {
-      "Job Title": "Data Analyst",
-      "Job Type": "Full-Time",
-      "Date": "2025-06-25",
-      "Salary": 55000,
-      "Location": "Lusaka, Zambia",
-      "Action": "View"
-    },
-    {
-      "Job Title": "Project Manager",
-      "Job Type": "Contract",
-      "Date": "2025-06-22",
-      "Salary": 75000,
-      "Location": "Abuja, Nigeria",
-      "Action": "View"
-    },
-    {
-      "Job Title": "QA Tester",
-      "Job Type": "Part-Time",
-      "Date": "2025-06-20",
-      "Salary": 40000,
-      "Location": "Accra, Ghana",
-      "Action": "View"
-    },
-    {
-      "Job Title": "System Administrator",
-      "Job Type": "Full-Time",
-      "Date": "2025-06-18",
-      "Salary": 62000,
-      "Location": "Kigali, Rwanda",
-      "Action": "View"
-    },
-    {
-      "Job Title": "Machine Learning Engineer",
-      "Job Type": "Full-Time",
-      "Date": "2025-06-15",
-      "Salary": 90000,
-      "Location": "Johannesburg, South Africa",
-      "Action": "View"
-    }
-  ];
+  columns: any [] = ["Job Title", "Job Type", "Date", "Salary", "Location", "Action"]
+
+  jobs = [{
+    "Job Title": "dgkjdh",
+    "Job Type": "sjkfd",
+    "Date": "2025-12-11",
+    "Salary": 5454,
+    "Location": "Accra",
+    "Action": "View"
+  }];
 
 
   openJobDetailsFormModal() {
@@ -115,7 +44,21 @@ export class EmployerJobsComponent {
 
 
   openJobCreationModal() {
+    this.modalService.createOrUpdateJobActionType = 'Create'
     this.modalService.openCreateJobFormModal()
+  }
+
+
+  handleOpenDeleteModal(event: boolean) {
+    // alert("open delete modal")
+    console.log( event )
+    this.showDeleteModal = true
+  }
+
+
+  handleCloseDeleteModal( event: boolean ) {
+    console.log( event )
+    this.showDeleteModal = false
   }
 
 
