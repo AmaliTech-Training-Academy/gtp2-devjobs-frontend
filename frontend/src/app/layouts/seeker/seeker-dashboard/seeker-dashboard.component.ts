8d377776f1ca5bbc700c693cd02d252bb6a2051b
Jummai <jummyhassan5@gmail.com>
2025-07-20 08:44:27 +0000

    conditionally remove tabs and seach component on seeker web layout

--- frontend/src/app/layouts/seeker/seeker-dashboard/seeker-dashboard.component.ts
+++ frontend/src/app/layouts/seeker/seeker-dashboard/seeker-dashboard.component.ts
@@ -1,30 +1,45 @@
 import { Component } from '@angular/core';
 import { SearchComponent } from '../../../components/search/search.component';
-import { RouterLink, RouterOutlet, ActivatedRoute, Router, NavigationEnd, RouterLinkActive } from '@angular/router';
+import {
+  RouterLink,
+  RouterOutlet,
+  ActivatedRoute,
+  Router,
+  NavigationEnd,
+  RouterLinkActive,
+} from '@angular/router';
 import { filter, map, startWith } from 'rxjs/operators';
 import { Observable, combineLatest } from 'rxjs';
-import { AsyncPipe } from '@angular/common';
+import { AsyncPipe, CommonModule } from '@angular/common';
 import { SeekerNavComponent } from '../../../features/jobs/seeker-nav/seeker-nav.component';
 
 @Component({
   selector: 'app-seeker-dashboard',
   standalone: true,
-  imports: [SearchComponent, SeekerNavComponent, RouterOutlet, RouterLink, AsyncPipe, RouterLinkActive],
+  imports: [
+    SearchComponent,
+    SeekerNavComponent,
+    RouterOutlet,
+    RouterLink,
+    AsyncPipe,
+    RouterLinkActive,
+    CommonModule,
+  ],
   templateUrl: './seeker-dashboard.component.html',
-  styleUrl: './seeker-dashboard.component.scss'
+  styleUrl: './seeker-dashboard.component.scss',
 })
 export class SeekerDashboardComponent {
   showSearch$: Observable<boolean>;
+  search: boolean = false;
 
   constructor(private route: ActivatedRoute, private router: Router) {
+    this.search = this.route.snapshot.data['search'];
     this.showSearch$ = combineLatest([
       this.router.events.pipe(
-        filter(event => event instanceof NavigationEnd),
+        filter((event) => event instanceof NavigationEnd),
         startWith(null)
       ),
-      this.route.firstChild?.data ?? this.route.data
-    ]).pipe(
-      map(([_, data]) => data['showSearch'] !== false)
-    );
+      this.route.firstChild?.data ?? this.route.data,
+    ]).pipe(map(([_, data]) => data['showSearch'] !== false));
   }
 }
