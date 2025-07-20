94c6ee04d008ea1d37331350a696f2a82c9710b0
Jummai <jummyhassan5@gmail.com>
2025-07-20 21:20:37 +0000

    Feature/fatima/profile settings (#78)
    * modify job response model to be generic
    
    * create a reusable getTimeAgo function in utils
    
    * display single job in job details view
    Added API method to retrieve a job by ID
    Integrated the method into the job details component
    Displaying job descriptions and metadata in the UI
    Please enter the commit message for your changes. Lines starting
    
    * fixed conflicts
    
    * add validations to password reset
    
    * conditionally remove tabs and seach component on seeker web layout
    
    * fixed responsiveness issue on profile and account managemet pages
    
    * add visibilty icons to password fields in account page
    
    * add disabled bg color to disabled input field
    
    * add back button and job title to application form
    
    * modify profile form data sent to backend to update profile
    
    * add http request to get company profile details and update
    
    * add request methods to get skills from server

--- frontend/src/app/features/seeker-account-management/seeker-account-management.component.ts
+++ frontend/src/app/features/seeker-account-management/seeker-account-management.component.ts
@@ -2,6 +2,8 @@ import { Component, inject } from '@angular/core';
 import { AccountManagementComponent } from '../../shared/account-management/account-management.component';
 import { Router } from '@angular/router';
 import { BackButtonComponent } from '../../shared/back-button/back-button.component';
+import { JobService } from '../../core/services/job-service/job.service';
+import { Skill } from '../../model/all.jobs';
 
 @Component({
   selector: 'app-seeker-account-management',
@@ -11,8 +13,31 @@ import { BackButtonComponent } from '../../shared/back-button/back-button.compon
 })
 export class SeekerAccountManagementComponent {
   router = inject(Router);
+  jobService = inject(JobService);
+  skills!: Skill[];
 
   onProfilePage() {
     this.router.navigate(['/seeker/dashboard/profile']);
+  }
+
+  constructor() {
+    this.jobService.getSkills().subscribe({
+      next: (data) => {
+        this.skills = data;
+      },
+      error: (err) => {
+        this.skills = [
+          {
+            id: 'c1234567-abcd-1234-ef00-1234567890ab',
+            createdAt: '2025-07-20T15:30:00.000Z',
+            updatedAt: '2025-07-20T16:00:00.000Z',
+            name: 'Java Programming',
+            category: 'Software',
+            usageCount: 42,
+            createdByUserId: 'b9876543-cdef-4567-abcd-0987654321fe',
+          },
+        ];
+      },
+    });
   }
 }
--- frontend/src/app/features/seeker-profile/seeker-profile.component.ts
+++ frontend/src/app/features/seeker-profile/seeker-profile.component.ts
@@ -1,7 +1,10 @@
-import { Component, inject } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
 import { ProfileComponent } from '../../shared/profile/profile.component';
 import { Router } from '@angular/router';
 import { BackButtonComponent } from '../../shared/back-button/back-button.component';
+import { JobService } from '../../core/services/job-service/job.service';
+import { ProfileData } from '../../model/all.jobs';
+import { log } from 'console';
 
 @Component({
   selector: 'app-seeker-profile',
@@ -9,10 +12,36 @@ import { BackButtonComponent } from '../../shared/back-button/back-button.compon
   templateUrl: './seeker-profile.component.html',
   styleUrl: './seeker-profile.component.scss',
 })
-export class SeekerProfileComponent {
+export class SeekerProfileComponent implements OnInit {
   router = inject(Router);
+  jobService = inject(JobService);
+  profileData!: ProfileData;
+
+  ngOnInit(): void {
+    this.jobService.getProfileDetails().subscribe({
+      next: (data) => {
+        this.profileData = data.data;
+        console.log(this.profileData);
+      },
+    });
+  }
 
   onSkillsPage() {
     this.router.navigate(['/seeker/dashboard/account-management']);
+  }
+
+  onSubmit(formData: FormData) {
+    console.log('form data to parent', formData);
+
+    this.jobService
+      .updateProfileDetails(formData, this.profileData.profileId)
+      .subscribe({
+        next: (response) => {
+          console.log(response);
+        },
+        error: (err) => {
+          console.log(err);
+        },
+      });
   }
 }
